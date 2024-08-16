193b59d351c772c497aaf6d9685df88518810b9b
Rishabh0717 <rishbhavats608@gmai.com>
2024-08-16 12:40:29 +0530

    my files

---
+++ print_statement.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+
+myname = "Rishabh"
+
+print(f"Hello {myname}! How are you?")
+
+def hello_name(somename):
+    print(f"Hello {somename}! This is a function.")
+
+def repeat_hello(somename, n_times):
+    for i in range(n_times):
+        print(f"Hello there {somename}! This is print statement number: {i+1}")
+
+
+if __name__=="__main__":
+    hello_name("Class")
+    hello_name("Rishabh")
+    hello_name("Folks")
+
+    repeat_hello(somename="Rishabh", n_times=7)
